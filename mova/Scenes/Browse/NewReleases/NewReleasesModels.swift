f9b7888b41d8b7991059b09433d4a0c5de337b1d
ewjinx <ranaoverhere@gmail.com>
2026-05-11 17:56:59 +0500

    feat: Add Top 10 and New Releases list views

---
+++ mova/Scenes/Browse/NewReleases/NewReleasesModels.swift
@@ -0,0 +1,16 @@
+//
+//  Top10Models.swift
+//  mova
+//
+//  Created by Muhammad Ahsan Rana on 5/5/2026.
+//
+
+import Foundation
+
+
+enum NewReleases {
+    struct ViewModel
+    {
+        let movies : [Home.FetchMovies.ViewModel.DisplayedMovie]
+    }
+}
